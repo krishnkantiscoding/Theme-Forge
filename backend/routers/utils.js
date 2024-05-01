6fabe2c2c58ecf3375f8c19cb8a43c1059295218
m-triple-m <triplem656@gmail.com>
2024-05-01 18:57:14 +0530

    ..

---
+++ backend/routers/utils.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const archiver = require('archiver');
+
+function zipDirectory(source, out) {
+    const archive = archiver('zip', { zlib: { level: 9 }}); // Sets the compression level.
+    const stream = fs.createWriteStream(out);
+
+    return new Promise((resolve, reject) => {
+        archive
+            .directory(source, false)
+            .on('error', err => reject(err))
+            .pipe(stream)
+        ;
+
+        stream.on('close', () => resolve());
+        archive.finalize();
+    });
+}
+
+// Usage:
+zipDirectory('../sampleExtension', './output.zip')
+    .then(() => console.log('Folder successfully zipped!'))
+    .catch(console.error);
